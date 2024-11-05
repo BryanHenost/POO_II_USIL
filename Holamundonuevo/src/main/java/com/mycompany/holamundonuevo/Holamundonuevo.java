9c29dea26e7173e6eb87617c5c13d418258bbfdc
BryanHenost <bryan.henost@gmail.com>
2024-11-05 17:28:04 +0100

    Se creo el archivo
    el archivo fue creado

---
+++ Holamundonuevo/src/main/java/com/mycompany/holamundonuevo/Holamundonuevo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundonuevo;
+
+/**
+ *
+ * @author bryan
+ */
+public class Holamundonuevo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello New World!");
+    }
+}
